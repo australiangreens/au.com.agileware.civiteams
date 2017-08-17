b1fc4564146b71ffee62fd263a5a7d421f8e4908
Francis Whittle <francis@agileware.com.au>
2017-08-17 15:08:56 +1000

    GREENS-10: Remove CRM_Team_BAO_Team::create boilerplate.

--- CRM/Team/BAO/Team.php
+++ CRM/Team/BAO/Team.php
@@ -2,23 +2,6 @@
 
 class CRM_Team_BAO_Team extends CRM_Team_DAO_Team {
 
-  /**
-   * Create a new Team based on array-data
-   *
-   * @param array $params key-value pairs
-   * @return CRM_Team_DAO_Team|NULL
-   *
-  public static function create($params) {
-    $className = 'CRM_Team_DAO_Team';
-    $entityName = 'Team';
-    $hook = empty($params['id']) ? 'create' : 'edit';
 
-    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
-    $instance = new $className();
-    $instance->copyValues($params);
-    $instance->save();
-    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
 
-    return $instance;
-  } */
 }
