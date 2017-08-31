63ce1a20c032a7d4ab9230eeffc304d2f21f5013
Francis Whittle <francis@agileware.com.au>
2017-08-31 17:33:34 +1000

    GREENS-10: Make create & get API calls respectively encode & decode JSON data field.

--- api/v3/Team.php
+++ api/v3/Team.php
@@ -20,6 +20,9 @@ function _civicrm_api3_team_create_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_team_create($params) {
+  if(is_array($params['data']) || is_object($params['data'])) {
+    $params['data'] = json_encode($params['data']);
+  }
   return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
 }
 
@@ -42,5 +45,14 @@ function civicrm_api3_team_delete($params) {
  * @throws API_Exception
  */
 function civicrm_api3_team_get($params) {
-  return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+  $teams = _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+  if(is_array($teams['values'])) {
+    foreach($teams['values'] as $id => &$team) {
+      if(!empty($team['data'])) {
+        $team['data'] = json_decode($team['data'], TRUE);
+      }
+    }
+  }
+
+  return $teams;
 }
