efa92e2d964ae73bc06688b5af05559e8a06be1c
Justin Freeman <justin@agileware.com.au>
2022-11-07 16:20:28 +1100

    CIVIBLD-287 Re-generate civix

--- civiteams.php
+++ civiteams.php
@@ -12,17 +12,6 @@ function civiteams_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function civiteams_civicrm_xmlMenu(&$files) {
-  _civiteams_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -75,56 +64,6 @@ function civiteams_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function civiteams_civicrm_managed(&$entities) {
-  _civiteams_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function civiteams_civicrm_caseTypes(&$caseTypes) {
-  _civiteams_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function civiteams_civicrm_angularModules(&$angularModules) {
-_civiteams_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function civiteams_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _civiteams_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_civicrm_entityTypes().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
@@ -169,9 +108,8 @@ function civiteams_civicrm_permission(&$permissions) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function civiteams_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
@@ -201,3 +139,21 @@ function civiteams_civicrm_navigationMenu(&$menu) {
 function civiteams_civicrm_alterAPIPermissions($entity, $action, &$params, &$permissions) {
   $permissions['team']['get'] = array ('or' => array('access civiteams', 'administer civiteams'));
 }
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function civiteams_civicrm_postInstall() {
+  _civiteams_civix_civicrm_postInstall();
+}
+
+// /**
+//  * Implements hook_civicrm_entityTypes().
+//  *
+//  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+//  */
+// function civiteams_civicrm_entityTypes(&$entityTypes) {
+//   _civiteams_civix_civicrm_entityTypes($entityTypes);
+// }
