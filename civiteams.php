5ba8854d306eec269066df664be1f348089508fa
Seamus Lee <seamuslee001@gmail.com>
2023-01-19 09:55:28 +1100

    Fix civiteams entitytypes hook to match standard practice

--- civiteams.php
+++ civiteams.php
@@ -69,13 +69,7 @@ function civiteams_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
  */
 function civiteams_civicrm_entityTypes(&$entityTypes) {
-  $entityFiles = _civiteams_civix_find_files(__DIR__, '*.entityType.php');
-  foreach ($entityFiles as $file) {
-    $et = include $file;
-    foreach ($et as $e) {
-      $entityTypes[$e['class']] = $e;
-    }
-  }
+  _civiteams_civix_civicrm_entityTypes($entityTypes);
 }
 
 /**
